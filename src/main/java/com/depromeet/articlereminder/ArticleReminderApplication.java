c352c973e4304953a008d40f00b8281fd01d6e15
junshock5 <topojs8@likelion.org>
2021-04-01 02:46:33 +0900

    create project

---
+++ src/main/java/com/depromeet/articlereminder/ArticleReminderApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.articlereminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleReminderApplication.class, args);
+	}
+
+}
