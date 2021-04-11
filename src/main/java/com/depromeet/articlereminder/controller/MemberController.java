903c59823c124443d8b8b1f695e4782bb60fb441
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-04-11 15:58:12 +0900

    junshock5 - make pojo

---
+++ src/main/java/com/depromeet/articlereminder/controller/MemberController.java
@@ -0,0 +1,7 @@
+package com.depromeet.articlereminder.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+}
---
+++ src/main/java/com/depromeet/articlereminder/service/MemberService.java
@@ -0,0 +1,7 @@
+package com.depromeet.articlereminder.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
